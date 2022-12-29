78dbc2e1a1c9e2e7f3bf2d5b8df014d77cdccce6
Ali Cangül <cangulali1@gmail.com>
2022-12-29 15:40:30 +0300

    design edit

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const container = document.querySelector(".container");
+const selected = document.querySelectorAll(".selected");
+const select = document.getElementById("movie");
+let count;
 
+container.addEventListener("click", function (e) {
+  e.target.classList.toggle("selected");
+  calculateTotal();
+});
+select.addEventListener("change", function (e) {
+  calculateTotal();
+});
+function calculateTotal() {
+  let selectedSeat = container.querySelectorAll(".seat.selected").length;
+  count = selectedSeat;
+  let value = select.value;
+  let price = count * value;
+  if (value != selected.value) {
+    console.log("puh");
+  }
+  document.querySelector("#amount").textContent = price;
+}
