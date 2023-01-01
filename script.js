e7688432495d61d53a640e4ca6c034ffdf6ca395
Ali Cangül <cangulali1@gmail.com>
2023-01-01 18:25:09 +0300

    setitem to localstorage added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
-const selected = document.querySelectorAll(".selected");
 const select = document.getElementById("movie");
+const seats = document.querySelectorAll(".seat:not(.reserved) ");
 let count;
 
 container.addEventListener("click", function (e) {
@@ -11,12 +11,31 @@ select.addEventListener("change", function (e) {
   calculateTotal();
 });
 function calculateTotal() {
-  let selectedSeat = container.querySelectorAll(".seat.selected").length;
-  count = selectedSeat;
+  let selectedSeat = container.querySelectorAll(".seat.selected");
+  count = selectedSeat.length;
   let value = select.value;
   let price = count * value;
-  if (value != selected.value) {
-    console.log("puh");
-  }
+
+  let seatArr = [];
+  let selectedArr = [];
+
+  seats.forEach(function (seat) {
+    seatArr.push(seat);
+  });
+  selectedSeat.forEach(function (seat) {
+    selectedArr.push(seat);
+  });
+  let selectedSeatIndex = selectedArr.map(function (seat) {
+    return seatArr.indexOf(seat);
+  });
+  console.log(selectedSeatIndex);
+
   document.querySelector("#amount").textContent = price;
+  document.getElementById("count").textContent = selectedSeat.length;
+  saveLocalStorage(selectedSeatIndex);
+}
+
+function saveLocalStorage(index) {
+  localStorage.setItem("selectedSeat", JSON.stringify(index));
+  localStorage.setItem("selectedMovie",select.selectedIndex)
 }
